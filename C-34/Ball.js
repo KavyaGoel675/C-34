6405c6889c30c8d70dae6a40fed78030b34deff1
KavyaGoel675 <78598510+KavyaGoel675@users.noreply.github.com>
2021-05-29 17:10:57 +0530

    Add files via upload

---
+++ C-34/Ball.js
@@ -0,0 +1,21 @@
+class Ball{
+    constructor(x,y,w,h,angle){
+        var options={
+            'frictionAir':0.005,
+            'density':1
+        }
+        this.body=Bodies.rectangle(x,y,w,h,options);
+        this.w=w;
+        this.h=h;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position;
+        var angle=this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        ellipse(0,0,this.w,this.h);
+        pop();
+    }
+}
